85496f98852add84468ba51f13225229b3cda0d7
Rakshanda Giri <rakshandagiri123@gmail.com>
2022-10-22 21:39:51 +0530

    htttract 1st page edited

--- winkthemes.tech/themes/templatemonster/theme-particle/email-send.php
+++ winkthemes.tech/themes/templatemonster/theme-particle/email-send.php
@@ -1,15 +1,17 @@
-<?php
+<!-- //?php
 // the message
 // $msg = "First line of text\nSecond line of text";
-if(isset($_REQUEST['send'])){
-    $name=$_REQUEST['name'];
-    $email=$_REQUEST['email'];
-    $sub=$_REQUEST['subject'];
-    $message=$_REQUEST['message'];
-    mail("palakn2410@gmail.com",$sub,$message,$email);
-}
-header("Location:index.html");
+// if(isset($_REQUEST['send'])){
+//     $name=$_REQUEST['name'];
+//     $email=$_REQUEST['email'];
+//     $sub=$_REQUEST['subject'];
+//     $message=$_REQUEST['message'];
+//     mail("palakn2410@gmail.com",$sub,$message,$email);
+// }
+// header("Location:index.html");
 //$comments = wordwrap($comments,70);
 // send email
+?> -->
+<?php
+echo "hello";
 ?>
-
